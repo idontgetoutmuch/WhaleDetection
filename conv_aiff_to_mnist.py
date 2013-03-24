f8d60fa6c79ff9b73a1dc7234d609b6b766e65e4
Dominic Steinitz <dominic@steinitz.org>
2013-03-24 09:34:17 +0000

    Complete the conversion of aiff to the format which the nn code understands.

--- conv_aiff_to_mnist.py
+++ conv_aiff_to_mnist.py
@@ -5,28 +5,17 @@ import aifc
 import numpy as np
 from matplotlib.mlab import specgram
 
-training_cases = {}
-whale_cases = []
-no_whale_cases = []
-
-test_cases=[i for i in range(1,54504)]
-
-with open("%s/data/train.csv"%WHALE_HOME, 'r') as csvfile:
-    label_reader = csv.reader(csvfile)
-    label_reader.next() # drop the headings
-    for index, (file_name, label) in enumerate(label_reader):
-        index = int(index)
-        training_cases[index] = (file_name, 1 if label == '1' else 0)
-        if label == '1':
-            whale_cases.append(index)
-        else:
-            no_whale_cases.append(index)
+def to_MNIST(n):
+    spec_data = get_spectrogram(n)
+    int_data = spec_data.astype(int)
+    return int_data
 
 long_specgram = lambda s: specgram(s, detrend=mpl.mlab.detrend_mean, NFFT=256, Fs=2, noverlap=178)
 
 def get_spectrogram(n, training=True):
     data = get_training_case(n, True, training)
     s,f,t = long_specgram(data)
+    s = 255.0 * s / np.max(np.abs(s))
     return s
 
 def load_aiff(filename):
@@ -45,7 +34,7 @@ def get_training_case(n, normalised=True, training=True):
             filename = "%s/data/train/%s" % (WHALE_HOME, training_cases[n][0])
             s = load_aiff(filename)
             if normalised:
-                s = s/float(np.max(np.abs(s)))
+                s = s / float(np.max(np.abs(s)))
                 return s
     else:
         if n < 1 or n > 54503:
@@ -56,3 +45,27 @@ def get_training_case(n, normalised=True, training=True):
             if normalised:
                 s = s/float(np.max(np.abs(s)))
                 return s
+
+training_cases = {}
+whale_cases = []
+no_whale_cases = []
+
+test_cases=[i for i in range(1,54504)]
+
+with open("%s/data/train.csv"%WHALE_HOME, 'r') as csvfile:
+    label_reader = csv.reader(csvfile)
+    label_reader.next() # drop the headings
+    for index, (file_name, label) in enumerate(label_reader):
+        index = int(index)
+        training_cases[index] = (file_name, 1 if label == '1' else 0)
+        if label == '1':
+            whale_cases.append(index)
+        else:
+            no_whale_cases.append(index)
+
+for n in range(1, 1001):
+    fn = 'data/train/MNIST%d.txt' % n
+    with open(fn, "w") as output:
+        mns = to_MNIST(n)
+        np.savetxt(output, mns, fmt='%d')
+
